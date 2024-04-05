ebb445939bedbe322f8456f0bab1c48a6d00c896
geraldkisilu <muligerald6@gmail.com>
2024-04-05 14:02:59 +0300

    latest

--- challenge-3/question3.js
+++ challenge-3/question3.js
@@ -2,7 +2,7 @@
 //How would you implement this function efficiently, ensuring that it accurately identifies prime numbers and returns them in the same order they appear in the input array? 
 //For example if you provide an array [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] it should return an array [2, 3, 5, 7].
 
-
+// Declare a function(isPrime)
 function isPrime(num) {
     if (num <= 1) {
         return false;
@@ -16,10 +16,14 @@ function isPrime(num) {
 }
 console.log(isPrime);
 
+// Declare a function(findPrimeNumbers)
+// function takes an array of numbers as input and filters out only the prime numbers using the isPrime function
 function findPrimeNumbers(arr) {
+    // returns a new array containing only the prime numbers from the original array, preserving their order
     return arr.filter(num => isPrime(num));
 }
 console.log(findPrimeNumbers);
+// Declare a function(filterAndDisplay)
 
 function filterAndDisplay() {
     const inputString = document.getElementById('numbersInput').value;
